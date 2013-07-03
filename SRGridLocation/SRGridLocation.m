278fd70753b6dd419f3f6d18ec4a6f9710fb5743
Sebastian Rehnby <sebastian@rehnby.com>
2013-07-03 21:57:03 +0200

    Added licence to files

--- SRGridLocation/SRGridLocation.m
+++ SRGridLocation/SRGridLocation.m
@@ -1,9 +1,30 @@
 //
-//  SRGridLocation.m
-//  Transit
+//  CLLocationGrid is based on the original Javascript code by Arnold Andreasson,
+//  available at
+//  http://mellifica.se/geodesi/gausskruger.js
 //
-//  Created by Sebastian Rehnby on 7/3/13.
-//  Copyright (c) 2013 Sebastian Rehnby. All rights reserved.
+//  and the SwedishGrid project by ICE House, available at
+//  http://github.com/icehouse/swedishgrid
+//
+//  Copyright (c) 2013 Sebastian Rehnby
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import "SRGridLocation.h"
